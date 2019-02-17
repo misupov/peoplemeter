62e3b4d02b08c2ec38a7e91cfbaec7475b46c79c
Mikhail Isupov <lam0x86@gmail.com>
2019-02-18 02:41:27 +0300

    no message

--- PikaWeb/Program.cs
+++ PikaWeb/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 namespace PikaWeb
 {
@@ -20,12 +23,23 @@ namespace PikaWeb
             return WebHost.CreateDefaultBuilder(args)
                 .UseKestrel(options =>
                 {
-                    options.Listen(IPAddress.Any, 443, listenOptions => { listenOptions.UseHttps("cert.pfx", "asd123"); });
+                    options.Listen(IPAddress.Any, 443, listenOptions =>
+                    {
+                        var httpsOptions = new HttpsConnectionAdapterOptions {ServerCertificateSelector = ServerCertificateSelector};
+                        listenOptions.UseHttps(httpsOptions);
+                    });
                     options.Listen(IPAddress.Any, 80);
                 })
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>();
+        }
+
+        private static X509Certificate2 ServerCertificateSelector(ConnectionContext arg1, string arg2)
+        {
+            Console.Out.WriteLine("CERT REQUEST");
+            var certificate = new X509Certificate2("cert.pfx", "asd123");
+            return certificate;
         }
     }
 }
