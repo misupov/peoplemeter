33ad59568ea8be4a60269827bd80bd6d7681f213
Mikhail Isupov <lam0x86@gmail.com>
2019-02-20 22:48:37 +0300

    no message

--- LetsEncrypt/LetsEncryptExtensions.cs
+++ LetsEncrypt/LetsEncryptExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 using Certes;
 using Certes.Acme;
@@ -14,6 +16,8 @@ namespace LetsEncrypt
 {
     public static class LetsEncryptExtensions
     {
+        private static readonly string LetsEncryptAccountPem = "lets-encrypt-account.pem";
+
         private static X509Certificate2 _certificate;
 
         public static ListenOptions UseLetsEncrypt(this ListenOptions options)
@@ -28,7 +32,7 @@ namespace LetsEncrypt
         {
             await Task.Delay(5000);
             var acme = new AcmeContext(WellKnownServers.LetsEncryptV2);
-            var account = await acme.NewAccount("lam0x86@gmail.com", true);
+            //var account = GetAccount("lam0x86@gmail.com");
 
             var order = await acme.NewOrder(new[] {"peoplemeter.ru"});
 
@@ -63,6 +67,25 @@ namespace LetsEncrypt
             var password = "abcd1234";
             _certificate = new X509Certificate2(pfxBuilder.Build("cert", password), password);
             Console.Out.WriteLine("Certificate saved.");
+        }
+
+        private static async Task<IAccountContext> GetAccount(string email)
+        {
+            try
+            {
+                var pemKey = await File.ReadAllTextAsync(LetsEncryptAccountPem, Encoding.ASCII);
+                var accountKey = KeyFactory.FromPem(pemKey);
+                var acme = new AcmeContext(WellKnownServers.LetsEncryptV2, accountKey);
+                return await acme.Account();
+            }
+            catch
+            {
+                var acme = new AcmeContext(WellKnownServers.LetsEncryptV2);
+                var account = await acme.NewAccount(email, true);
+                var pemKey = acme.AccountKey.ToPem();
+                await File.WriteAllTextAsync(LetsEncryptAccountPem, pemKey, Encoding.ASCII);
+                return account;
+            }
         }
 
         private static X509Certificate2 ServerCertificateSelector(ConnectionContext arg1, string arg2)
