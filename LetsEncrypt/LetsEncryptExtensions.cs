8ae564b558795e43fb940b8f95ca740804b112f5
Mikhail Isupov <lam0x86@gmail.com>
2019-02-21 23:14:17 +0300

    no message

--- LetsEncrypt/LetsEncryptExtensions.cs
+++ LetsEncrypt/LetsEncryptExtensions.cs
@@ -30,69 +30,76 @@ namespace LetsEncrypt
 
         private static async Task RenewCertificate()
         {
-            await Task.Delay(5000);
-            Console.Out.WriteLine("Logging in to LetsEncrypt");
-            var acme = new AcmeContext(WellKnownServers.LetsEncryptV2);
-            var account = await GetAccount("lam0x86@gmail.com");
-            Console.Out.WriteLine("Logged in to LetsEncrypt");
+            Console.Out.WriteLine("[LetsEncrypt] Logging in");
+            var server = WellKnownServers.LetsEncryptStagingV2;
+            var (acme, account) = await GetAccount("lam0x86@gmail.com", server);
+            Console.Out.WriteLine("[LetsEncrypt] Logged in");
 
-            var order = await acme.NewOrder(new[] {"peoplemeter.ru"});
-            Console.Out.WriteLine("[LetsEncrypt] new order created");
-
-            var authz = (await order.Authorizations()).First();
-            Console.Out.WriteLine("[LetsEncrypt] Authorizations passed");
-            var httpChallenge = await authz.Http();
-            Console.Out.WriteLine("[LetsEncrypt] http challenge ok");
-            var token = httpChallenge.Token;
-            var keyAuthz = httpChallenge.KeyAuthz;
-            AcmeChallengeTokensStorage.AddToken(token, keyAuthz);
-            Console.Out.WriteLine("Validate token");
-            await Task.Delay(10000);
-            var challenge = await httpChallenge.Validate();
-            while (challenge.Status != ChallengeStatus.Valid)
+            try
             {
-                Console.Out.WriteLine("Re-validate token");
+                Console.Out.WriteLine("[LetsEncrypt] new order created");
+                var order = await acme.NewOrder(new[] {"peoplemeter.ru"});
+                Console.Out.WriteLine("[LetsEncrypt] new order created");
+
+                var authz = (await order.Authorizations()).First();
+                Console.Out.WriteLine("[LetsEncrypt] Authorizations passed");
+                var httpChallenge = await authz.Http();
+                Console.Out.WriteLine("[LetsEncrypt] http challenge ok");
+                var token = httpChallenge.Token;
+                var keyAuthz = httpChallenge.KeyAuthz;
+                AcmeChallengeTokensStorage.AddToken(token, keyAuthz);
+                Console.Out.WriteLine("Validate token");
                 await Task.Delay(10000);
-                challenge = await httpChallenge.Validate();
+                var challenge = await httpChallenge.Validate();
+                while (challenge.Status != ChallengeStatus.Valid)
+                {
+                    Console.Out.WriteLine("Re-validate token");
+                    await Task.Delay(10000);
+                    challenge = await httpChallenge.Validate();
+                }
+
+                Console.Out.WriteLine("Saving the Certificate.");
+                var privateKey = KeyFactory.NewKey(KeyAlgorithm.ES256);
+                var cert = await order.Generate(new CsrInfo
+                {
+                    CountryName = "CA",
+                    State = "Ontario",
+                    Locality = "Toronto",
+                    Organization = "Certes",
+                    OrganizationUnit = "Dev",
+                    CommonName = "peoplemeter.ru",
+                }, privateKey);
+
+                var pfxBuilder = cert.ToPfx(privateKey);
+                var password = "abcd1234";
+                _certificate = new X509Certificate2(pfxBuilder.Build("cert", password), password);
+                Console.Out.WriteLine("Certificate saved.");
             }
-
-            Console.Out.WriteLine("Saving the Certificate.");
-            var privateKey = KeyFactory.NewKey(KeyAlgorithm.ES256);
-            var cert = await order.Generate(new CsrInfo
+            catch (Exception e)
             {
-                CountryName = "CA",
-                State = "Ontario",
-                Locality = "Toronto",
-                Organization = "Certes",
-                OrganizationUnit = "Dev",
-                CommonName = "peoplemeter.ru",
-            }, privateKey);
 
-            var pfxBuilder = cert.ToPfx(privateKey);
-            var password = "abcd1234";
-            _certificate = new X509Certificate2(pfxBuilder.Build("cert", password), password);
-            Console.Out.WriteLine("Certificate saved.");
+            }
         }
 
-        private static async Task<IAccountContext> GetAccount(string email)
+        private static async Task<(AcmeContext, IAccountContext)> GetAccount(string email, Uri server)
         {
             try
             {
-                var pemKey = await File.ReadAllTextAsync(LetsEncryptAccountPem, Encoding.ASCII);
+                var pemKey = await File.ReadAllTextAsync($"{LetsEncryptAccountPem}-{email}", Encoding.ASCII);
                 var accountKey = KeyFactory.FromPem(pemKey);
-                var acme = new AcmeContext(WellKnownServers.LetsEncryptV2, accountKey);
+                var acme = new AcmeContext(server, accountKey);
                 Console.Out.WriteLine("[LetsEncrypt] Account restored");
-                return await acme.Account();
+                return (acme, await acme.Account());
             }
             catch
             {
                 Console.Out.WriteLine("[LetsEncrypt] Creating new account");
-                var acme = new AcmeContext(WellKnownServers.LetsEncryptV2);
+                var acme = new AcmeContext(server);
                 var account = await acme.NewAccount(email, true);
                 var pemKey = acme.AccountKey.ToPem();
-                await File.WriteAllTextAsync(LetsEncryptAccountPem, pemKey, Encoding.ASCII);
+                await File.WriteAllTextAsync($"{LetsEncryptAccountPem}-{email}", pemKey, Encoding.ASCII);
                 Console.Out.WriteLine("[LetsEncrypt] Account created");
-                return account;
+                return (acme, account);
             }
         }
 
