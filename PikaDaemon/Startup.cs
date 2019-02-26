0db8e0a817bd915f4194d2e47f7d913dddb6756d
Mikhail Isupov <lam0x86@gmail.com>
2019-02-26 22:10:16 +0300

    no message

--- PikaDaemon/Startup.cs
+++ PikaDaemon/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PikaDaemon.Data;
-using WebApplication7.Data;
 
 namespace PikaDaemon
 {
