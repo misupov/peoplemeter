b157d0a90604347a4a02585b8d79ae21e1f7668f
Mikhail Isupov <lam0x86@gmail.com>
2019-02-26 22:23:37 +0300

    no message

--- PikaDaemon/Startup.cs
+++ PikaDaemon/Startup.cs
@@ -37,6 +37,24 @@ namespace PikaDaemon
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                // options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                // options.Lockout.MaxFailedAccessAttempts = 5;
+                // options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                // options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
